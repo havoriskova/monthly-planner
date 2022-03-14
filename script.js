cf62705c6c5e0680d178d5d530c5fd952501733c
havoriskova <havoriskova@gmail.com>
2022-03-14 19:46:28 +0100

    refactoring of javascript
    - instead of many functions which make changes in preview according to the form, NOW is there an object previewLook, in which are all the methods
    - object "previewLook" includes a method "setDefaultForm", from which you can set default values for the form/preview - according to this I also changed CSS and HTML a bit

--- script.js
+++ script.js
@@ -61,197 +61,229 @@ window.addEventListener("resize", changeWidth);
 changeWidth();
 // ---------------- konec nastavení výšky
 
+// začátek
 
-// ---------------- JSON - vygenerovat kalendář přímo v js kvůli počtu řádků
-
+const root = document.querySelector(":root");
+const preview = document.getElementById("preview");
+const generatorForm = document.getElementById("generator-form");
 const calendar = document.getElementById("calendar");
-const [m1, m2, m3, m4, m5, m6, m7, m8, m9, m10, m11, m12] = ["january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"];
 const formYear = document.getElementById("form-year");
 let previewYear = document.getElementById("name-of-year");
-
-let selectedYear = 2022; // defaultně
-
-function changeYear(e) {
- 
-    selectedYear = parseInt(e.target.value);
-    previewYear.innerText = selectedYear;
-    fetching(false);
-}
-
-formYear.addEventListener("input", changeYear);
-
-function fetching(isItFirstTime) {
-fetch("http://localhost:3000/years.json") // app.js  http://localhost:3000/years.json 
-// netlify https://hungry-mirzakhani-0f7c44.netlify.app/years.json
-.then(response => {
-    return response.json();
-})
-.then(jsondata => useData(jsondata, isItFirstTime));
-}
-
-fetching(true); //initial call
-
-function useData(years, isItFirstTime) {
-
-let daysBefore = years[`${selectedYear}`].january[1];
-let days = years[`${selectedYear}`].january[2];
-let daysAfter = years[`${selectedYear}`].january[3];
-
-if (!isItFirstTime) {calendar.innerHTML= ``;}
-
-    for(let i = daysBefore[0]; i <= daysBefore[1]; i++) {
-        generateNumbers(i, true);
-    }
-
-    for(let i = days[0]; i <= days[1]; i++) {
-        generateNumbers(i, false);
-    }
-
-    for(let i = daysAfter[0]; i <= daysAfter[1]; i++) {
-      generateNumbers(i, true);
-    }
-}
-
-
-function generateNumbers (i, isGray){
-    
-    let day = document.createElement("span");
-        day.innerHTML = i;
-        calendar.appendChild(day);
-        day.classList.add("grid-child");
-
-        if(isGray){
-        day.classList.add("gray-numbers");
-    } else if(!isGray) {
-        day.classList.add("changing-color");
-        }
-   
-
-}
-
-// ---------------- konec JSON
-
-
-// ---------------- jak stáhnout pdf  - MDN Blob
-// let href = URL.createObjectURL(new Blob([], {type: "application/pdf"}));
-// to mi hodí http adresu, na které bude ten soubor ke stažení
-// po použití linku je dobré link smazat URL.revokeObjectURL(href);
-
-// ---------------- konec jak stáhnout pdf
-
-
-// ---------------- FORM - notes
-
-const formNotes = document.getElementById("form-notes");
-const notes = document.getElementById("notes");
-
-function changeNotes(e) {
-notes.classList.toggle("no-notes");
-}
-
-formNotes.addEventListener("input", changeNotes);
-
-// ---------------- konec notes
-
-// ---------------- FORM - font 
-
-const preview = document.getElementById("preview");
-const formFonts = document.querySelectorAll(`input[name="font"]`);
-
-function changeFont(e) {
-
-if (formFonts[1].checked) {
-    preview.style.fontFamily = `serif`;
-} else if (formFonts[2].checked) {
-    preview.style.fontFamily = `sans-serif`;
-}
-else if (formFonts[0].checked) {
-    preview.style.fontFamily = `Bitter`;
-}
-}
-
-formFonts.forEach(font => font.addEventListener("input", changeFont));
-
-// ---------------- konec font
-
-// ---------------- FORM - color BOOOM
-
-const color = document.getElementById("color");
-const root = document.querySelector(":root");
-
-function changeColor(e) {
-    root.style.setProperty(`--color-for-preview`, e.target.value);
-}
-
-color.addEventListener("input", changeColor);
-
-// ---------------- konec color
-
-// ---------------- FORM - language
-
-let formLanguages = document.querySelectorAll(`input[name="language"]`);
-let daysOfWeekPreview = document.querySelectorAll(".day-of-week");
-
-const months = {
- czech: ["leden", "únor", "březen", "duben", "květen", "červen", "červenec", "srpen", "září", "říjen", "listopad", "prosinec"] ,
- dutch:  ["januari", "februari", "maart", "april", "mei", "juni", "juli", "augustus", "september", "oktober", "november", "december"],
- english: ["january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"]
-}
-
-const daysOfWeek = {
- czech: ["pondělí", "úterý", "středa", "čtvrtek", "pátek", "sobota", "neděle"],
- dutch: ["maandag", "dinsdag", "woensdag", "donderdag", "vrijdag", "zaterdag", "zondag"],
- english: ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"]
-}
-
-function changeMonth(lang) {
-let previewMonth = document.getElementById("name-of-month");
-
-previewMonth.innerHTML =  months[lang][0]; 
-}
-
-function changeWeek(lang) {
-
-for(let i = 0; i < 7; i++) {
-daysOfWeekPreview[i].innerHTML = daysOfWeek[lang][i];
-}
-}
-
-function changeLanguage(e) {
-let language = e.path[0].id; // language je string
-
-changeMonth(language);
-changeWeek(language);
-}
-
-formLanguages.forEach(formLang => formLang.addEventListener("input", changeLanguage));
-
-// ---------------- konec language
-
-// ---------------- FORM orientation - toggle .orientation-portrait
 let orientationForm = document.querySelectorAll(`input[name="orientation"]`);
 const previewOrientation = document.querySelector("#preview-orientation");
-
-function changeOrientation() {
-previewOrientation.classList.toggle("orientation-landscape");    
-previewOrientation.classList.toggle("orientation-portrait");
-}
-
-orientationForm.forEach(orient => orient.addEventListener("input", changeOrientation));
-
-// ---------------- konec orientation
-
-// ---------------- defaultní values pro form, musí být až dole po všech funkcích
-function setDefaultForm() {
-    changeMonth("english");
-    changeWeek("english");
-}
-
-setDefaultForm();
-// ---------------- konec nastavení defaultu 
+let formLanguages = document.querySelectorAll(`input[name="language"]`);
+let daysOfWeekPreview = document.querySelectorAll(".day-of-week");
+let previewMonth = document.getElementById("name-of-month");
+const color = document.getElementById("input-color");
+const formNotes = document.getElementById("form-notes");
+const notes = document.getElementById("notes");
+const formFonts = document.querySelectorAll(`input[name="font"]`);
 
 
-const generatorForm = document.getElementById("generator-form");
+
+let previewLook = { 
+    
+        defaultYear: 2025,
+        defaultColor : "#008800", // value color inputu vyžaduje hexa formát
+        defaultOrientation : "portrait",
+        defaultFont : "bitter",
+        defaultLanguage : "czech",
+        defaultNotes : "on",
+
+        setDefaultForm: function() {
+            this.changeLanguage();
+            this.changeYear();
+            this.changeColor();
+            this.changeNotes();
+            this.changeFont();
+            this.changeOrientation();
+        },
+
+
+        changeYear: function(e) {
+            let selectedYear; 
+
+            if (!e) {
+                selectedYear = previewLook.defaultYear;
+                previewYear.innerText = selectedYear;
+                fetching(true); // = načíst kalendář
+                // option s value previewLook.defaultYear  
+                // .setAttribute("selected", ""); 
+
+                let option = document.querySelector(`option[value="${previewLook.defaultYear}"]`);
+                console.log(option);
+                option.setAttribute("selected", "");
+            
+            } else {
+            selectedYear = parseInt(e.target.value);
+            previewYear.innerText = selectedYear;
+            fetching(false); // = znovu načíst kalendář
+            }
+
+
+            function fetching(isItFirstTime) {
+
+            fetch("https://hungry-mirzakhani-0f7c44.netlify.app/years.json")   // netlify https://hungry-mirzakhani-0f7c44.netlify.app/years.json // http://127.0.0.1:8887/years.json "200 ok" // 5500 live server // app.js 3000
+            .then(response => {
+                return response.json();
+            })
+            .then(jsondata => useData(jsondata, isItFirstTime));
+            }
+
+
+            function useData(years, isItFirstTime) {
+           
+            let daysBefore = years[`${selectedYear}`].january[1];
+            let days = years[`${selectedYear}`].january[2];
+            let daysAfter = years[`${selectedYear}`].january[3];
+
+            if (!isItFirstTime) {calendar.innerHTML= ``;}
+
+                for(let i = daysBefore[0]; i <= daysBefore[1]; i++) {
+                    generateNumbers(i, true);
+                }
+
+                for(let i = days[0]; i <= days[1]; i++) {
+                    generateNumbers(i, false);
+                }
+
+                for(let i = daysAfter[0]; i <= daysAfter[1]; i++) {
+                generateNumbers(i, true);
+                }
+            }
+
+                function generateNumbers (i, isGray){
+                
+                    let day = document.createElement("span");
+                    day.innerHTML = i;
+                    calendar.appendChild(day);
+                    day.classList.add("grid-child");
+
+                    if(isGray){
+                    day.classList.add("gray-numbers");
+                    } else if(!isGray) {
+                    day.classList.add("changing-color");
+                    }
+            }
+        },
+
+        changeLanguage: function(e) {
+
+            const months = {
+            czech: ["leden", "únor", "březen", "duben", "květen", "červen", "červenec", "srpen", "září", "říjen", "listopad", "prosinec"] ,
+            dutch:  ["januari", "februari", "maart", "april", "mei", "juni", "juli", "augustus", "september", "oktober", "november", "december"],
+            english: ["january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"]
+            }
+
+            const daysOfWeek = {
+            czech: ["pondělí", "úterý", "středa", "čtvrtek", "pátek", "sobota", "neděle"],
+            dutch: ["maandag", "dinsdag", "woensdag", "donderdag", "vrijdag", "zaterdag", "zondag"],
+            english: ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"]
+            }
+
+
+            let language;
+            if (!e) {language = previewLook.defaultLanguage;
+            let lang = document.getElementById(previewLook.defaultLanguage);
+            lang.setAttribute("checked", "");
+
+            }  else {language = e.path[0].id} // -> string
+            
+            changeMonth(language);
+            changeWeek(language);
+
+                function changeMonth(language) {
+                previewMonth.innerHTML =  months[language][0]; 
+                }
+
+                function changeWeek(language) {
+                for(let i = 0; i < 7; i++) {
+                daysOfWeekPreview[i].innerHTML = daysOfWeek[language][i];
+                }
+                }       
+
+        },
+
+        changeColor: function(e) {
+            if(!e) {
+                root.style.setProperty(`--color-for-preview`, previewLook.defaultColor);
+                color.setAttribute("value", `${previewLook.defaultColor}`);
+            } else {
+                root.style.setProperty(`--color-for-preview`, e.target.value);
+            color.setAttribute("value", e.target.value)}
+        },
+
+        changeOrientation: function(e) {
+            if (!e && previewLook.defaultOrientation === "landscape") {
+                // v CSS mám defaultně nastavený class landscape na preview
+
+                let orient = document.getElementById(previewLook.defaultOrientation);
+                orient.setAttribute("checked", "");
+
+            } else if (!e && previewLook.defaultOrientation === "portrait") {
+
+                let orient = document.getElementById(previewLook.defaultOrientation);
+                orient.setAttribute("checked", "");
+
+                previewOrientation.classList.toggle("orientation-landscape");    
+                previewOrientation.classList.toggle("orientation-portrait"); 
+            
+            } else {
+
+                previewOrientation.classList.toggle("orientation-landscape");    
+                previewOrientation.classList.toggle("orientation-portrait"); 
+            }
+        },
+
+        changeFont: function(e) {
+
+            if(!e) {
+                let font = document.getElementById(previewLook.defaultFont);
+                font.setAttribute("checked", "");
+                preview.style.fontFamily = previewLook.defaultFont;
+
+            } else {
+
+                if (formFonts[1].checked) {
+                    preview.style.fontFamily = `serif`;
+                } else if (formFonts[2].checked) {
+                    preview.style.fontFamily = `sans-serif`;
+                }
+                else if (formFonts[0].checked) {
+                    preview.style.fontFamily = `bitter`;
+                } }
+        },
+
+        changeNotes: function(e){
+            if(!e && previewLook.defaultNotes == "on") {
+            formNotes.setAttribute("checked", "");
+            notes.classList.remove("no-notes");
+            } else {
+            
+            notes.classList.toggle("no-notes");}
+        }
+
+        }
+
+
+ //--------------
+
+
+previewLook.setDefaultForm(); // ten zahrnuje všechny initial calls
+
+formYear.addEventListener("input", previewLook.changeYear);
+formLanguages.forEach(formLang => formLang.addEventListener("input", previewLook.changeLanguage));
+orientationForm.forEach(orient => orient.addEventListener("input", previewLook.changeOrientation));
+color.addEventListener("input", previewLook.changeColor);
+formNotes.addEventListener("input", previewLook.changeNotes);
+formFonts.forEach(font => font.addEventListener("input", previewLook.changeFont));
+
+
+// ---------------- konec
+
+
+
+
 
 function handleSubmit(e) {
  e.preventDefault();
